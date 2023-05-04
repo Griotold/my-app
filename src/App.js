15f61c2979287a6562812df8a9c6e99ad51a7d5d
Griotold <gotjd9773@naver.com>
2023-05-04 15:26:28 +0900

    jsx 기본 문법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 1. return 시에 하나의 Dom만 리턴 가능
+// 2. 변수 선언은 let 혹은 const로만!
+// 3. if 사용 불가 -> 삼항 연산자는 사용 가능
+// 4. 조건부 렌더링 -> (조건 && 값(true))
+// 5. css디자인
+
+let a = 10;
+const b = 20;
+
 function App() {
-    return <div>안녕asd</div>;
+    let c;
+    // console.log(1, c);
+
+    const mystyle = {
+        color: 'magenta',
+    };
+
+    return (
+        <div>
+            <div style={mystyle}>
+                안녕 {a === 10 ? '10입니다.' : '10이 아닙니다.'}
+            </div>
+            <h1 className="box-style">헤딩태그 {b === 20 && '20입니다.'}</h1>
+            <hr />
+        </div>
+    );
 }
 
 export default App;
