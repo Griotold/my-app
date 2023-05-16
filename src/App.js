3552b6b9b3c71105029a57f159613c1305b3b4c7
Griotold <gotjd9773@naver.com>
2023-05-16 11:39:57 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-    let list = [1, 2, 3];
+    // let number = 1; // 상태 값이 아님
+    const [number, setNumber] = useState(1);
+    const add = () => {
+        setNumber(number + 1);
+        console.log('add', number);
+    };
 
+    // 렌더링 시점 = 상태값이 변경 될 때
     return (
         <div>
-            <div>
-                {list.map((n) => (
-                    <h1>{n}</h1>
-                ))}
-            </div>
+            <h1>숫자 : {number}</h1>
+            <button onClick={add}>더하기</button>
         </div>
     );
 }
