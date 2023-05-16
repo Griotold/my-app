f07fb32f36ed88f41ffc300c4338fdb098636e9e
Griotold <gotjd9773@naver.com>
2023-05-16 12:04:27 +0900

    useState 2 - 배열

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-    // let number = 1; // 상태 값이 아님
-    const [number, setNumber] = useState(1);
-    const add = () => {
-        setNumber(number + 1);
-        console.log('add', number);
+    const [users, setUsers] = useState([]);
+
+    const download = () => {
+        let sample = [
+            { id: 1, name: '홍길동' },
+            { id: 2, name: '길복순' },
+            { id: 3, name: '황용식' },
+            { id: 4, name: '동백이' },
+        ];
+        setUsers([...users, ...sample]);
     };
 
     // 렌더링 시점 = 상태값이 변경 될 때
     return (
         <div>
-            <h1>숫자 : {number}</h1>
-            <button onClick={add}>더하기</button>
+            <button onClick={download}>다운로드</button>
+            {users.map((u) => (
+                <h1>
+                    {u.id}, {u.name}
+                </h1>
+            ))}
         </div>
     );
 }
