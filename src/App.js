22fde46b478af3a9d0c2ac2aeacd1e9ec40e3079
Griotold <gotjd9773@naver.com>
2023-05-19 13:57:08 +0900

    props 변수 passing하기 완료

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import React, { useEffect, useState, useMemo, useRef, createRef } from 'react';
 
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
     return (
         <div>
-            <LoginPage />
+            <HomePage />
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 // 하나의 컴포넌트를 생성 -> 재사용 하려고
 // styled-components => js와 css를 하나의 파일로 관리
 // 해당 컴포넌트는 이 파일만 보면 된다.
-const FooterList = styled.div`
+const StyledFooterDiv = styled.div`
     border: 1px solid black;
     height: 300px;
 `;
 
 const Footer = () => {
     return (
-        <FooterList>
+        <StyledFooterDiv>
             <ul>
                 <li>오시는 길 : 서울시 강남구...</li>
                 <li>전화번호 : 02032052</li>
             </ul>
-        </FooterList>
+        </StyledFooterDiv>
     );
 };
 
