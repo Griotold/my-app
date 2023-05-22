e5abb657422f72689b27ae9bdae7df26581ab882
Griotold <gotjd9773@naver.com>
2023-05-22 12:51:25 +0900

    라우팅 해결, 글쓰기+글목록보기+글삭제(1/2)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState, useMemo, useRef, createRef } from 'react';
 
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
-
+import { Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import ListPage from './pages/ListPage';
+import WritePage from './pages/WritePage';
 function App() {
     return (
         <div>
-            <Header />
-
-            <Route path="/" exact={true} component={HomePage} />
-            <Route path="/login/:id" exact={true} component={LoginPage} />
-
-            <Footer />
+            <Navigation />
+            <Switch>
+                <Route path="/" exact component={ListPage} />
+                <Route path="/write" exact component={WritePage} />
+            </Switch>
         </div>
     );
 }
