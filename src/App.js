c81e04974d6a698c7ccc04e4d0dbc1dc604d76eb
Griotold <gotjd9773@naver.com>
2023-05-24 14:01:48 +0900

    글 목록 보기 쓰기 완료

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
+
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
+
 function App() {
     return (
         <div>
-            <Navigation />
-            <Switch>
-                <Route path="/" exact component={ListPage} />
-                <Route path="/write" exact component={WritePage} />
-            </Switch>
+            <ListPage />
         </div>
     );
 }
